c5021f4130db76ad4360a8a25429db3da4830168
Colton0012 <coltond0012@gmail.com>
2022-02-16 22:54:40 -0800

    completing assets, metadata, and css section

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,15 @@
 import Link from "next/link"
-
+import Head from "next/head"
+import Layout
+ from "../../components/layout"
 export default function FirstPost(){
     return (
-        <>
+        <Layout>
+            <Head>
+                <title>
+                    First Post
+                </title>
+            </Head>
             <h1>
                 First Post Page
             </h1>
@@ -11,6 +18,6 @@ export default function FirstPost(){
                 <a>back to home</a>
                 </Link>
             </h2>
-        </>
+        </Layout>
     )
 }
